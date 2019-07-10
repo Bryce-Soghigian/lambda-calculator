6d71ca316da9e1950e07b92d1531b8731ee577dd
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-07-10 12:56:48 -0500

    refactored for hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "./App.css";
 import Button from "./components/Button"
 import {Input} from "./components/Input"
@@ -10,59 +10,55 @@ import  * as math from 'mathjs';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      input: ''
-    }
+
+  function App(props){
+    const [input,setInput] = useState();
+   const addToInput = val => {
+    setInput(input + val);
   }
-  addToInput = val => {
-    this.setState({input: this.state.input + val});
-  }
-  handleEqual = () => {
-    this.setState({input: math.evaluate(this.state.input)})
-  }
-  
-  render(){
+ const handleEqual = () => {
+    // this.setState({input: math.evaluate(this.useState)})
+  setInput(input, math.evaluate(input))
+
+  };
     return(
-      <div className="App">
+<div className="App">
       <div className="calc-wrapper">
-        <Input input={this.state.input}></Input>
+        <Input input= {useState()}></Input>
         <div className = "SpecialRow">
-          <ClearButton handleClear = {() => this.setState({input:''})}>C</ClearButton>
+          <ClearButton handleClear = {() => setInput('')}>C</ClearButton>
           <Button>x/-</Button>
           <Button>%</Button>
           <Button>/</Button>
         </div>
         <div className = "row">
-        <Button handleClick={this.addToInput}> 1</Button>
-        <Button handleClick={this.addToInput}>2</Button>
-        <Button handleClick={this.addToInput}>3</Button>
-        <Button handleClick={this.addToInput}>*</Button>
+        <Button handleClick={addToInput()}> 1</Button>
+        <Button handleClick={addToInput()}>2</Button>
+        <Button handleClick={addToInput()}>3</Button>
+        <Button handleClick={addToInput()}>*</Button>
         </div>
         <div className = "row">
-        <Button handleClick={this.addToInput}>4</Button>
-        <Button handleClick={this.addToInput}>5</Button>
-        <Button handleClick={this.addToInput}>6</Button>
-        <Button handleClick={this.addToInput}>-</Button>
+        <Button handleClick={addToInput()}>4</Button>
+        <Button handleClick={addToInput()}>5</Button>
+        <Button handleClick={addToInput()}>6</Button>
+        <Button handleClick={addToInput()}>-</Button>
         </div>
         <div className = "row">
-        <Button handleClick={this.addToInput}>7</Button>
-        <Button handleClick={this.addToInput}>8</Button>
-        <Button handleClick={this.addToInput}> 9</Button>
-        <Button handleClick={this.addToInput}>+</Button>
+        <Button handleClick={addToInput()}>7</Button>
+        <Button handleClick={addToInput()}>8</Button>
+        <Button handleClick={addToInput()}> 9</Button>
+        <Button handleClick={addToInput()}>+</Button>
         </div>
         <div className = "row">
-        <Button handleClick={this.addToInput}>0</Button>
-        <Button handleClick={this.addToInput}>.</Button>
+        <Button handleClick={addToInput()}>0</Button>
+        <Button handleClick={addToInput()}>.</Button>
         
-        <Button handleClick={() => this.handleEqual()}>=</Button>
+        <Button handleClick={() => handleEqual()}>=</Button>
         </div>
       </div>
       </div>
-    );
+    )
   }
-}
+
 
 export default App;
